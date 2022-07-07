c599d6dc24b94b4472ee45d1af5d65e4c7d8e160
Anastasiia Konovalenko <anastasiiak@coherentsolutions.com>
2022-07-07 10:14:10 +0300

    feat:

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Router, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import MainPage from "./pages/main_page/MainPage";
 import ProductPage from "./pages/product_page/Page";
 import ProductCard from "./pages/product_page/ProductCard";
